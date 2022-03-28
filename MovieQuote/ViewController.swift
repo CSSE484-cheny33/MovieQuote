a340debc5497b12d1171876967f4d5e5e536055d
Yutong Chen <chney33@rose-hulman.edu>
2022-03-28 08:12:02 -0400

    Initial Commit

---
+++ MovieQuote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieQuote
+//
+//  Created by Yutong Chen on 3/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
